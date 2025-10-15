af6bbc15dd170a8da21dba652bf48345511ebe13
ASEP-PC <saeful.rochmat240302@gmail.com>
2025-10-15 09:48:03 +0700

    Belajar Typescript OOP bagian Visibility

--- test/visibility.test.ts
+++ test/visibility.test.ts
@@ -2,24 +2,44 @@ describe ('Visibility Class', function() {
     // Visibility di typeScript ada 3, yaitu public, private, dan protected
     class Counter {
         // default visibility adalah public
-        public counter: number = 0;
+        // properti dengan visibility private, hanya bisa diakses dari dalam class
+        // prperti dengan visibility protected, hanya bisa diakses dari dalam class dan turunannya
+        protected counter: number = 0;
 
-        // method dengan visibility private, hanya bisa diakses dari dalam class
-        private increment(): void  {
+        
+        public increment(): void  {
             this.counter++;
         }
 
-        // method dengan visibility protected, hanya bisa diakses dari dalam class dan turunannya
+        
         public getCounter(): number {
             return this.counter;
         }
     }
 
+    class DoubleCounter extends Counter {
+        public increment(): void {
+            this.counter += 2; // tidak bisa mengakses counter karena visibilitynya private, tapi bisa di akses jika visibilitynya protected
+        }
+    }
+
     it('Should can access private', function() {
-        // Bisa diakses dari luar class, karena visibilitynya public
         const counter: Counter = new Counter();
-        // counter.increment(); // error, karena increment adalah private
-        counter.counter = 10; // bisa, karena counter adalah public
+        // counter.counter = 100; // error, karena counter adalah private
+
+        counter.increment(); // bisa mengakses increment karena visibilitynya public
+        counter.increment(); // bisa mengakses increment karena visibilitynya public
+        counter.increment(); // bisa mengakses increment karena visibilitynya public
+        console.info(counter.getCounter());
+    })
+
+    it('Should can access protected', function() {
+        const counter: DoubleCounter = new DoubleCounter();
+        // counter.counter = 100; // error, karena counter adalah protected
+
+        counter.increment(); // bisa mengakses increment karena visibilitynya public
+        counter.increment(); // bisa mengakses increment karena visibilitynya public
+        counter.increment(); // bisa mengakses increment karena visibilitynya public
         console.info(counter.getCounter());
     })
 })
