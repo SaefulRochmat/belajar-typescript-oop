f7fa092f7b38ebc1f5609ebdbe3bb99866c7f2b2
ASEP-PC <saeful.rochmat240302@gmail.com>
2025-10-15 09:25:27 +0700

    Belajar Typescript OOP bagian Super Method

---
+++ test/supermethod.test.ts
@@ -0,0 +1,26 @@
+describe ('super method', () => {
+    class Employee {
+        name: string;
+        constructor(name: string) {
+            this.name = name;
+        }
+        sayHello(name: string): void {
+            console.info(`Hello ${name}, my name is ${this.name}`);
+        }
+    }
+
+    class Manager extends Employee {
+        sayHello(name: string): void {
+            super.sayHello(name);
+            console.info(`Good day ${name}, my name is ${this.name}, I am the manager here.`);
+        }
+    }
+
+    it('Should can call super method', function() {
+        const employee: Employee = new Employee("Saeful");
+        employee.sayHello("Asep");  
+
+        const manager: Manager = new Manager("Umar");
+        manager.sayHello("Asep");
+    })
+})
